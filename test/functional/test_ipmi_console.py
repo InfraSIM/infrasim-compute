88a4a6add1a76175104400632b5b09706a44c7bb
helloarys <helloarys@gmail.com>
2016-07-19 03:41:59 -0400

    Change the test case's format into the subclass of unittest. Put the ssh and channel connect and close method into setup and teardown func. Delete the unnecessary '\n' command after each channel.send(). Check the return result to make sure that the sel info has been recorded.

--- test/functional/test_ipmi_console.py
+++ test/functional/test_ipmi_console.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+import unittest
 import subprocess
 import re
 import os
@@ -36,137 +37,102 @@ def read_buffer(channel):
     return str_output
 
 
-def setup_func():
-    socat.start_socat()
-    ipmi.start_ipmi('quanta_d51')
-    run_command('ipmi-console start &', True, None, None)
-    time.sleep(5)
-    
+class test_ipmi_console(unittest.TestCase):
 
-def teardown_func():
-    run_command('ipmi-console stop', True, None, None)
-    qemu.stop_qemu()
-    ipmi.stop_ipmi()
-    socat.stop_socat()
-
-
-@with_setup(setup_func, teardown_func)
-def test_sensor_accessibility():
+    ssh = paramiko.SSHClient()
+    channel = None
     # Just for quanta_d51
     sensor_id = '0xc0'
     sensor_value = '1000.00'
-
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('127.0.0.1', username='', password='', port=9300)
-
-    channel = ssh.invoke_shell()
-
-    channel.send('sensor info\n')
-    channel.send('\n')
-    time.sleep(0.1)
-    str_output = read_buffer(channel)
-    print str_output
-    assert str_output.find('degrees C') != -1
-
-    channel.send('sensor value get ' + sensor_id + '\n')
-    channel.send('\n')
-    time.sleep(0.1)
-    str_output = read_buffer(channel)
-    print str_output
-    assert str_output.find('Fan_SYS0') != -1
-
-    channel.send('sensor value set ' + sensor_id + ' ' + sensor_value + '\n')
-    time.sleep(0.1)
-
-    channel.send('sensor value get ' + sensor_id + '\n')
-    channel.send('\n')
-    time.sleep(0.1)
-    str_output = read_buffer(channel)
-    print str_output
-    assert str_output.find('Fan_SYS0 : 1000.000 RPM') != -1
-
-    channel.close()
-    ssh.close()
-
-
-@with_setup(setup_func, teardown_func)
-def test_help_accessibility():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('127.0.0.1', username='', password='', port=9300)
-
-    channel = ssh.invoke_shell()
-
-    channel.send('help\n')
-    channel.send('\n')
-    time.sleep(0.1)
-    str_output = read_buffer(channel)
-    assert str_output.find('Available') != -1
-
-    channel.close()
-    ssh.close()
-
-
-@with_setup(setup_func, teardown_func)
-def test_sel_accessibility():
-    # Just for quanta_d51
-    sensor_id = '0xc0'
     event_id = '6'
 
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('127.0.0.1', username='', password='', port=9300)
-
-    channel = ssh.invoke_shell()
-
-    channel.send('sel get ' + sensor_id + '\n')        
-    channel.send('\n')
-    time.sleep(0.1)
-    str_output = read_buffer(channel)
-    assert str_output.find('ID') != -1
-
-    channel.send('sel set ' + sensor_id + ' ' + event_id + ' assert\n')
-    channel.send('\n')
-    time.sleep(0.1)
-    channel.send('sel set ' + sensor_id + ' ' + event_id + ' deassert\n')
-    channel.send('\n')
-    time.sleep(0.1)
-
-    ipmi_sel_cmd = 'ipmitool -H 127.0.0.1 -U admin -P admin sel list'
-    returncode, output = run_command(ipmi_sel_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print output
-    assert returncode == 0
-
-    channel.close()
+    @classmethod
+    def setUpClass(cls):
+        socat.start_socat()
+        ipmi.start_ipmi('quanta_d51')
+        run_command('ipmi-console start &', True, None, None)
+        time.sleep(5)
+        cls.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        cls.ssh.connect('127.0.0.1', username='', password='', port=9300)
+        cls.channel = cls.ssh.invoke_shell()
+    
+    @classmethod
+    def tearDownClass(cls):
+        cls.channel.close()
+        cls.ssh.close()
+        run_command('ipmi-console stop', True, None, None)
+        qemu.stop_qemu()
+        ipmi.stop_ipmi()
+        socat.stop_socat()
 
 
-@with_setup(setup_func, teardown_func)
-def test_history_accessibilty():
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect('127.0.0.1', username='', password='', port=9300)
+    def test_sensor_accessibility(self):
+        self.channel.send('sensor info\n')
+        time.sleep(0.1)
+        str_output = read_buffer(self.channel)
+        print str_output
+        assert str_output.find('degrees C') != -1
+    
+        self.channel.send('sensor value get ' + self.sensor_id + '\n')
+        time.sleep(0.1)
+        str_output = read_buffer(self.channel)
+        print str_output
+        assert str_output.find('Fan_SYS0') != -1
+    
+        self.channel.send('sensor value set ' + self.sensor_id + ' ' + self.sensor_value + '\n')
+        time.sleep(0.1)
+    
+        self.channel.send('sensor value get ' + self.sensor_id + '\n')
+        time.sleep(0.1)
+        str_output = read_buffer(self.channel)
+        print str_output
+        assert str_output.find('Fan_SYS0 : 1000.000 RPM') != -1
+    
+    
+    def test_help_accessibility(self):
+        self.channel.send('help\n')
+        time.sleep(0.1)
+        str_output = read_buffer(self.channel)
+        assert str_output.find('Available') != -1
+    
+    
+    def test_sel_accessibility(self):
+        self.channel.send('sel get ' + self.sensor_id + '\n')        
+        time.sleep(0.1)
+        str_output = read_buffer(self.channel)
+        assert str_output.find('ID') != -1
+    
+        self.channel.send('sel set ' + self.sensor_id + ' ' + self.event_id + ' assert\n')
+        time.sleep(0.1)
+        self.channel.send('sel set ' + self.sensor_id + ' ' + self.event_id + ' deassert\n')
+        time.sleep(0.1)
+    
+        ipmi_sel_cmd = 'ipmitool -H 127.0.0.1 -U admin -P admin sel list'
+        returncode, output = run_command(ipmi_sel_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        lines = str(output).splitlines()
+        assert_line = lines[-2]
+        deassert_line = lines[-1]
+        print assert_line
+        print deassert_line
+        assert assert_line.find('Fan #0xc0 | Upper Non-critical going low  | Asserted') != -1
+        assert deassert_line.find('Fan #0xc0 | Upper Non-critical going low  | Deasserted') != -1
+    
+    
+    def test_history_accessibilty(self):
+        self.channel.send('help\n')        
+        time.sleep(0.1)
+        self.channel.send('help sensor\n')        
+        time.sleep(0.1)
+        self.channel.send('history\n')        
+        time.sleep(0.1)
+    
+        str_output = read_buffer(self.channel)
+        lines = str_output.splitlines()
+        print str_output
+    
+        assert lines[-4].find('help') != -1
+        assert lines[-3].find('help sensor') != -1
 
-    channel = ssh.invoke_shell()
-
-    channel.send('help\n')        
-    channel.send('\n')
-    time.sleep(0.1)
-    channel.send('help sensor\n')        
-    channel.send('\n')
-    time.sleep(0.1)
-    channel.send('history\n')        
-    time.sleep(0.1)
-
-    str_output = read_buffer(channel)
-    lines = str_output.splitlines()
-    print lines
-
-    assert lines[-4] == '0  help'
-    assert lines[-3] == '1  help sensor'
-
-    channel.close()
-    ssh.close()
 
 
 
