a36362f8762feef56fa8bf0f4b9188b15566e988
Robert Xia <robert.xia@emc.com>
2016-10-26 18:39:36 +0800

    Add 'kvm_enabled" in test template.

--- test/fixtures.py
+++ test/fixtures.py
@@ -1,10 +1,13 @@
 
+from infrasim import helper
+
 class FakeConfig(object):
     def __init__(self):
         self.__node_info = {
             "name": "test",
             "type": "quanta_d51",
             "compute": {
+                "kvm_enabled": helper.check_kvm_existence(),
                 "cpu": {
                     "quantities": 2,
                     "type": "Haswell"
