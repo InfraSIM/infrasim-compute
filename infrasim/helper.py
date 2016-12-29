5dde7095a137d742e91da5a07a393e518e0c0267
Robert Xia <robert.xia@emc.com>
2016-12-29 17:12:37 +0800

    Use libc API getifaddrs to get all interfaces.

--- infrasim/helper.py
+++ infrasim/helper.py
@@ -7,10 +7,17 @@ import os
 import socket
 from functools import wraps
 from ctypes import cdll
+from socket import AF_INET, AF_INET6, inet_ntop
+from ctypes import (
+    Structure, Union, POINTER,
+    pointer, get_errno, cast,
+    c_ushort, c_byte, c_void_p,
+    c_char_p, c_uint, c_uint16,
+    c_uint32
+)
 import fcntl
 import struct
-import array
-from infrasim import run_command, InfraSimError
+from infrasim import InfraSimError
 
 libc = cdll.LoadLibrary('libc.so.6')
 setns = libc.setns
@@ -53,31 +60,130 @@ def get_interface_ip(ifname):
     :param interface: interface name
     :return: empty string if no such interface, else IP address
     """
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        ifreq = struct.pack('16sH14s', ifname, AF_UNIX, '\x00'*14)
-        res = fcntl.ioctl(s.fileno(), SIOCGIFADDR, ifreq)
-        ip = struct.unpack('16sH2x4s8x', res)[2]
-        s.close()
-        return socket.inet_ntoa(ip)
-    except Exception:
-        return None
+    interfaces = _get_all_interfaces()
 
+    for intf_obj in interfaces:
+        if intf_obj.get_interface_name() == ifname:
+            return intf_obj.get_interface_ipv4_address()
+    return None
+
+
+class struct_ifaddrs(Structure):
+    pass
+
+
+class struct_sockaddr(Structure):
+    _fields_ = [
+        ('sa_family', c_ushort),
+        ('sa_data', c_byte * 14), ]
+
+
+class union_ifa_ifu(Union):
+    _fields_ = [
+        ('ifu_broadaddr', POINTER(struct_sockaddr)),
+        ('ifu_dstaddr', POINTER(struct_sockaddr)), ]
+
+
+class struct_sockaddr_in(Structure):
+    _fields_ = [
+        ('sin_family', c_ushort),
+        ('sin_port', c_uint16),
+        ('sin_addr', c_byte * 4)]
+
+
+class struct_sockaddr_in6(Structure):
+    _fields_ = [
+        ('sin6_family', c_ushort),
+        ('sin6_port', c_uint16),
+        ('sin6_flowinfo', c_uint32),
+        ('sin6_addr', c_byte * 16),
+        ('sin6_scope_id', c_uint32)]
+
+
+struct_ifaddrs._fields_ = [
+    ('ifa_next', POINTER(struct_ifaddrs)),
+    ('ifa_name', c_char_p),
+    ('ifa_flags', c_uint),
+    ('ifa_addr', POINTER(struct_sockaddr)),
+    ('ifa_netmask', POINTER(struct_sockaddr)),
+    ('ifa_ifu', union_ifa_ifu),
+    ('ifa_data', c_void_p), ]
+
+
+def getfamaddr(sa):
+    family = sa.sa_family
+    addr = None
+    if family == AF_INET:
+        sa = cast(pointer(sa), POINTER(struct_sockaddr_in)).contents
+        addr = inet_ntop(family, sa.sin_addr)
+    elif family == AF_INET6:
+        sa = cast(pointer(sa), POINTER(struct_sockaddr_in6)).contents
+        addr = inet_ntop(family, sa.sin6_addr)
+    return family, addr
+
+
+class NetworkInterface(object):
+    def __init__(self, name):
+        self.name = name
+        self.index = libc.if_nametoindex(name)
+        self.addresses = {}
+
+    def __str__(self):
+        return "%s [index=%d, IPv4=%s, IPv6=%s]" % (
+            self.name, self.index,
+            self.addresses.get(AF_INET),
+            self.addresses.get(AF_INET6))
+
+    def get_interface_name(self):
+        return self.name
+
+    def get_interface_ipv4_address(self):
+        return self.addresses.get(AF_INET)
+
+    def get_interface_ipv6_address(self):
+        return self.addresses.get(AF_INET6)
+
+    def get_interface_index(self):
+        return self.index
+
+
+def ifap_iter(ifap):
+    ifa = ifap.contents
+    while True:
+        yield ifa
+        if not ifa.ifa_next:
+            break
+        ifa = ifa.ifa_next.contents
+
+
+def _get_all_interfaces():
+    ifap = POINTER(struct_ifaddrs)()
+    result = libc.getifaddrs(pointer(ifap))
+    if result != 0:
+        raise OSError(get_errno())
+    del result
+
+    try:
+        retval = {}
+        for ifa in ifap_iter(ifap):
+            name = ifa.ifa_name
+            i = retval.get(name)
+            if not i:
+                i = retval[name] = NetworkInterface(name)
+            family, addr = getfamaddr(ifa.ifa_addr.contents)
+            if addr:
+                i.addresses[family] = addr
+        return retval.values()
+    finally:
+        libc.freeifaddrs(ifap)
 
 def get_all_interfaces():
-    bytes = 128 * 32
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    interface_names = array.array('B', '\0' * bytes)
-    inbytes = struct.pack('iL', bytes, interface_names.buffer_info()[0])
-    res = fcntl.ioctl(s.fileno(), SIOCGIFCONF, inbytes)
-    returned_bytes = struct.unpack('iL', res)[0]
-    interface_names_str = interface_names.tostring()
     intf_list = []
-    for i in range(0, returned_bytes, 40):
-        intfn = interface_names_str[i:i+16].split('\0', 1)[0]
-        intf_list.append(intfn)
-    return intf_list
+    all_interfaces = _get_all_interfaces()
+    for interface in all_interfaces:
+        intf_list.append(interface.get_interface_name())
 
+    return intf_list
 
 @run_in_namespace
 def ip4_addresses(netns=None):
