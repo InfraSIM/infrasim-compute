56ca21d8e6843227f5cbd03481fc1f804e39725b
Aaron Wei <aaron.wei@dell.com>
2018-04-10 01:59:58 +0000

    implement ivn test

--- test/fixtures.py
+++ test/fixtures.py
@@ -3,6 +3,7 @@ from infrasim import helper
 
 
 class FakeConfig(object):
+
     def __init__(self):
         self.__node_info = {
             "name": "test",
@@ -46,6 +47,7 @@ class FakeConfig(object):
 
 
 class NvmeConfig(object):
+
     def __init__(self):
         self.__node_info = {
             "name": "nvme",
@@ -118,3 +120,83 @@ class NvmeConfig(object):
 
     def get_node_info(self):
         return self.__node_info
+
+
+class IvnConfig(object):
+
+    def __init__(self):
+        self.__info = {
+            "namespace": [
+                "node0ns",
+                "node1ns"
+            ],
+            "ovs": [
+                "br-int"
+            ],
+            "connection": {
+                "ns0-einf0": "vint0",
+                "ns1-einf0": "vint1"
+            },
+            "node0ns": {
+                "bridges": [
+                    {
+                        "ifname": "br0",
+                        "type": "static",
+                        "netmask": "255.255.255.0",
+                        "bridge_ports": "ns0-einf0",
+                        "address": "192.168.188.91"
+                    }
+                ],
+                "interfaces": [
+                    {
+                        "ifname": "ns0-einf0",
+                        "type": "static",
+                        "netmask": "255.255.255.0",
+                        "address": "0.0.0.0"
+                    }
+                ]
+            },
+            "br-int": {
+                "netmask": "255.255.255.0",
+                "type": "static",
+                "ports": [
+                    "vint0",
+                    "vint1"
+                ],
+                "address": "192.168.188.90"
+            },
+            "node1ns": {
+                "bridges": [
+                    {
+                        "ifname": "br0",
+                        "type": "static",
+                        "netmask": "255.255.255.0",
+                        "bridge_ports": "ns1-einf0",
+                        "address": "192.168.188.92"
+                    }
+                ],
+                "interfaces": [
+                    {
+                        "ifname": "ns1-einf0",
+                        "type": "static",
+                        "netmask": "255.255.255.0",
+                        "address": "0.0.0.0"
+                    }
+                ]
+            },
+            "portforward": {
+                "rules": [
+                    "192.168.188.91 5901 15901",
+                    "192.168.188.92 5901 25901",
+                    "192.168.188.91 8022 18022",
+                    "192.168.188.92 8022 28022"
+                ],
+                "io_interfaces": [
+                    "ens160",
+                    "br-int"
+                ]
+            }
+        }
+
+    def get_ivn_info(self):
+        return self.__info
