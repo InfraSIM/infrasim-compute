16292798087573682b7f8e88df81d91f8929632c
Robert Xia <robert.xia@emc.com>
2018-04-14 22:00:55 +0800

    test: refine infrasim init test with package manager

--- infrasim/package_manager.py
+++ infrasim/package_manager.py
@@ -1,6 +1,11 @@
 import os
 import re
-import apt_pkg
+try:
+    import apt_pkg
+except ImportError:
+    from infrasim import run_command
+    run_command("apt-get install python-apt")
+    import apt_pkg
 import apt.cache
 import apt.progress.text
 import apt.progress.base
@@ -11,26 +16,28 @@ from infrasim.yaml_loader import YAMLLoader
 
 
 class PackageManager(object):
-    _debug = False
 
-    def __init__(self, source_list_entry):
+    def __init__(self, update_cache=True, source_list_entry=None, progress=False):
         apt_pkg.init_config()
         # _MUST_ called prior to apt_pkg initialization
         # single entry in sources.list file
         # such as 'deb https://<repo url> trusty main'
         self.__add_entry(source_list_entry)
 
-        # apt_pkg.init()
-        apt_pkg.init_system()
+        self.__progress = progress
+
+        self.__init_apt_pkg_cache()
+
+        if update_cache:
+            self.__init_apt_cache()
+
+    def __init_apt_pkg_cache(self):
+        apt_pkg.init()
         op_progress = None
-        acquire_progress = None
-        if PackageManager._debug:
+        if self.__progress:
             op_progress = apt.progress.text.OpProgress()
-            acquire_progress = apt.progress.text.AcquireProgress()
         self.__apt_pkg_cache = apt_pkg.Cache(op_progress)
-        self.__apt_pkg_cache.update(acquire_progress, apt_pkg.SourceList())
         self.__depcache = apt_pkg.DepCache(self.__apt_pkg_cache)
-        self.__init_apt_cache()
 
     def __add_entry(self, entry):
         """
@@ -74,19 +81,15 @@ class PackageManager(object):
         sources_list.save()
 
     def __init_apt_cache(self):
-        pm = apt_pkg.PackageManager(self.__depcache)
         op_progress = None
         acquire_progress = None
-        if PackageManager._debug:
+        if self.__progress:
             op_progress = apt.progress.text.OpProgress()
             acquire_progress = apt.progress.text.AcquireProgress()
 
         cache = apt.cache.Cache(op_progress)
         cache.update(acquire_progress)  # apt-get update
-        fetcher = apt_pkg.Acquire(apt.progress.base.AcquireProgress())
-        cache.fetch_archives(fetcher=fetcher)
-        cache.install_archives(pm, apt.progress.base.InstallProgress())
-        # cache.fetch_archives(progress=apt.progress.text.AcquireProgress())
+        cache.commit()
 
     def is_installed(self, package_name):
         """
@@ -112,39 +115,48 @@ class PackageManager(object):
         """
         acquire_progress = apt.progress.base.AcquireProgress()
         install_progress = apt.progress.base.InstallProgress()
+        target_pkg = None
+
+        for pkg in self.__apt_pkg_cache.packages:
+            if pkg.name == package_name:
+                target_pkg = pkg
+                break
 
         op_progress = None
-        if PackageManager._debug:
+        if self.__progress:
             op_progress = apt.progress.text.OpProgress()
         self.__depcache.init(op_progress)
-        if package_name not in self.__apt_pkg_cache:
+        if (package_name not in self.__apt_pkg_cache) or (target_pkg is None):
             print "{} not in cache, please check source.".format(package_name)
             return False
 
-        if version_str:
+        if version_str and version_str != "latest":
             # print self.__apt_pkg_cache[package_name].version_list
             target_version = None
             target_version = self.__get_version(package_name, version_str)
             if target_version:
-                self.__depcache.set_candidate_ver(self.__apt_pkg_cache[package_name],
+                self.__depcache.set_candidate_ver(target_pkg,
                                                   target_version)
 
-        self.__depcache.mark_install(self.__apt_pkg_cache[package_name])
+        self.__depcache.mark_install(target_pkg)
         if force and self.is_installed(package_name):
-            self.__depcache.set_reinstall(self.__apt_pkg_cache[package_name], True)
+            self.__depcache.set_reinstall(target_pkg, True)
 
         try:
             self.__depcache.commit(acquire_progress, install_progress)
-        except Exception:
-            print "{} is not installed".format(package_name)
+        except apt_pkg.Error as e:
+            print "{} is not installed (Reason: {})".format(package_name, e)
 
-        return self.__apt_pkg_cache[package_name].inst_state == apt_pkg.INSTSTATE_OK
+        return target_pkg.inst_state == apt_pkg.INSTSTATE_OK
 
     def do_uninstall(self, package_name):
+        self.__init_apt_pkg_cache()
         if self.is_installed(package_name):
-            self.__depcache.mark_delete(self.__apt_pkg_cache[package_name])
-            self.__depcache.commit(apt.progress.base.AcquireProgress(),
-                                   apt.progress.base.InstallProgress())
+            for pkg in self.__apt_pkg_cache.packages:
+                if pkg.name == package_name:
+                    self.__depcache.mark_delete(pkg, True)
+                    self.__depcache.commit(apt.progress.base.AcquireProgress(),
+                                           apt.progress.base.InstallProgress())
 
     def list_all_packages(self):
         for pkg in self.__apt_pkg_cache.packages:
@@ -160,7 +172,7 @@ def read_packages_info():
 
 
 def install_all_packages(force=True, entry=None):
-    pm = PackageManager(entry)
+    pm = PackageManager(source_list_entry=entry, progress=False)
     # install offical packages
     # don't have to install the depencies for each installation
     for pkg_name in ("socat", "ipmitool", "libssh-dev", "libffi-dev", "libyaml-dev"):
@@ -175,6 +187,7 @@ def install_all_packages(force=True, entry=None):
     for pkg_info in package_info_list:
         res = pm.do_install(pkg_info.get('name'), version_str=pkg_info.get('version'), force=force)
         print "Installing {}... {}".format(pkg_info.get('name'), "Done" if res else "Fail")
+
 
 if __name__ == '__main__':
     install_all_packages()
