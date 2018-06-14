3cb2bce4c574aded2103341ce7910ff851450be7
WangFanta <shuoshi.wang@emc.com>
2018-06-14 10:06:47 +0000

    add test about error inject on nvme drive

--- test/functional/test_drive_error_inject.py
+++ test/functional/test_drive_error_inject.py
@@ -8,30 +8,30 @@ import os
 import json
 from infrasim import model
 from infrasim import helper
+from infrasim import sshclient
 from test import fixtures
 from infrasim.helper import UnixSocket
 
 old_path = os.environ.get('PATH')
 new_path = '{}/bin:{}'.format(os.environ.get('PYTHONPATH'), old_path)
-image = os.environ.get('TEST_IMAGE_PATH') or "/home/infrasim/jenkins/data/ubuntu14.04.4.qcow2"
 
 conf = {}
 error_inject_list = [
-    'nvme -i dev-nvme-0 -n 1 -s 4 -t 0',
-    'nvme -i dev-nvme-0 -n 1 -s 5 -t 0',
-    'nvme -i dev-nvme-0 -n 1 -s 6 -t 0',
-    'nvme -i dev-nvme-0 -n 1 -s 18 -t 0',
-    'nvme -i dev-nvme-0 -n 1 -s 25 -t 0',
-    'nvme -i dev-nvme-0 -n 1 -s 130 -t 0',
-    'nvme -i dev-nvme-0 -n 1 -s 132 -t 0',
-    'nvme -i dev-nvme-0 -n 1 -s 128 -t 2',
-    'nvme -i dev-nvme-0 -n 1 -s 129 -t 2',
-    'nvme -i dev-nvme-0 -n 1 -s 130 -t 2',
-    'nvme -i dev-nvme-0 -n 1 -s 131 -t 2',
-    'nvme -i dev-nvme-0 -n 1 -s 132 -t 2',
-    'nvme -i dev-nvme-0 -n 1 -s 133 -t 2',
-    'nvme -i dev-nvme-0 -n 1 -s 134 -t 2',
-    'nvme -i dev-nvme-0 -n 1 -s 135 -t 2'
+    [4, 0, 'Data Transfer'],
+    [5, 0, 'Power Loss'],
+    [6, 0, 'Internal'],
+    [18, 0, 'Invalid Use of Controller Memory Buffer'],
+    [25, 0, 'Keep Alive Timeout Expired'],
+    [130, 0, 'Namespace Not Ready'],
+    [132, 0, 'Format In Progress'],
+    [128, 2, 'Write Fault'],
+    [129, 2, 'Unrecovered Read '],
+    [130, 2, 'End-to-end Guard Check'],
+    [131, 2, 'End-to-end Application Tag Check'],
+    [132, 2, 'End-to-end Reference Tag Check'],
+    [133, 2, 'Compare Failure'],
+    [134, 2, 'Access Denied'],
+    [135, 2, 'Deallocated or Unwritten Logical Block']
 ]
 
 
@@ -78,7 +78,8 @@ class test_error_inject(unittest.TestCase):
             cls.stop_node()
 
     def test_nvmeerror_inject(self):
-        ssh = helper.prepare_ssh()
+        ssh = sshclient.SSH(host="127.0.0.1", username="root", password="root", port=2222)
+        ssh.wait_for_host_up()
         s = UnixSocket(path)
         s.connect()
         s.recv()
@@ -87,28 +88,46 @@ class test_error_inject(unittest.TestCase):
         }
         s.send(json.dumps(payload_enable_qmp))
         s.recv()
-        for cmd_error_inject in error_inject_list:
-            payload_error_inject = {
-                "execute": "nvme-status-code-error-inject",
-                "arguments": {
-                    "command-line": cmd_error_inject
-                }
-            }
-
-            s.send(json.dumps(payload_error_inject))
-            s.recv()
-            stdin, stdout, stderr = ssh.exec_command('nvme read /dev/nvme0n1 -z 3008 -a 128')
-            self.assertNotIn('Success', stdout, "error inject fail")
 
         payload_error_inject = {
             "execute": "nvme-status-code-error-inject",
             "arguments": {
-                "command-line": 'nvmeclear'
+                "count": 65536,
+                "opcode": "rw",
+                "id": "dev-nvme-0",
+                "nsid": 1,
+                "status_field": {
+                    "dnr": True,
+                    "more": True
+                }
+            }
+        }
+        payload_nvmeclear = {
+            'execute': 'nvme-status-code-error-inject',
+            'arguments': {
+                'count': 0,
+                'opcode': 'rw',
+                'id': 'dev-nvme-0',
+                'nsid': 0,
+                'status_field': {
+                    'sc': 0,
+                    'sct': 0,
+                    'dnr': True,
+                    'more': True
+                }
             }
         }
 
-        s.send(json.dumps(payload_error_inject))
-        s.recv()
+        for cmd_error_inject in error_inject_list:
+            payload_error_inject['arguments']['status_field']['sc'] = cmd_error_inject[0]
+            payload_error_inject['arguments']['status_field']['sct'] = cmd_error_inject[1]
+            s.send(json.dumps(payload_error_inject))
+            s.recv()
+            status, output = ssh.exec_command("nvme read /dev/nvme0n1 -z 3008 -a 128")
+            self.assertNotIn("Success", output, "error of %s inject faile" % cmd_error_inject[2])
 
+            s.send(json.dumps(payload_nvmeclear))
+            s.recv()
+            status, output = ssh.exec_command("nvme read /dev/nvme0n1 -z 3008 -a 128")
+            self.assertIn("Success", output, "clear error faile")
         s.close()
-        ssh.close()
