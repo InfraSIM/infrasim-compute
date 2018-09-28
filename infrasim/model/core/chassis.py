68a2ca69416d73fd4ae321f05c3a5db4d9a247bc
Aaron Wei <aaron.wei@dell.com>
2018-09-28 08:57:09 +0000

    chassis: add field for heart_beat and nodes_power_status in share memory

--- infrasim/model/core/chassis.py
+++ infrasim/model/core/chassis.py
@@ -173,6 +173,14 @@ class CChassis(object):
             if "pn" in key or "sn" in key:
                 buf[key] = "{}".format(data[key]).encode()
 
+        # setup gpio section
+        node_nr = len(self.__chassis.get("nodes", []))
+        # power status of all nodes: 1 byte per node.
+        buf["nodes_power"] = '\0' * node_nr
+        # heart_beat: 1 byte per node.
+        buf["heart_beat"] = '\0' * node_nr
+
+        # setup led seciton.
         buf["led"] = ' ' * data.get("led", 20)
 
         self.__dataset.append("chassis", buf)
