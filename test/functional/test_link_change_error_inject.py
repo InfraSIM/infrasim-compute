bfb9578d997b3883b0d3459a699d6f3436815918
Aaron Wei <aaron.wei@dell.com>
2019-02-20 10:26:12 +0000

    test_link: refactor code to avoid instability

--- test/functional/test_link_change_error_inject.py
+++ test/functional/test_link_change_error_inject.py
@@ -6,20 +6,14 @@ Copyright @ 2018 EMC Corporation All Rights Reserved
 import unittest
 import os
 import json
-import time
-import base64
 from infrasim import model
-from infrasim.helper import UnixSocket
-from infrasim import sshclient
+from infrasim.helper import UnixSocket, prepare_ssh, ssh_exec, ssh_close
 from test import fixtures
+from time import sleep
 
 
-conf = {}
 old_path = os.environ.get("PATH")
 new_path = "{}/bin:{}".format(os.environ.get("PYTHONPATH"), old_path)
-inface = " "
-QEMU_PATH = "https://bintray.com/infrasim/deb/download_file?file_path\
-=infrasim-qemu_2.10.1-ubuntu-xenial-1.0.239_amd64.deb"
 
 
 def setup_module():
@@ -27,32 +21,41 @@ def setup_module():
 
 
 def teardown_module():
-    global conf
-    if conf:
-        stop_node()
+    stop_node()
     os.environ["PATH"] = old_path
+
+
+conf = None
+target_mac = "00:60:16:9c:50:6a"
+s = None
 
 
 def start_node():
     global conf
     global ssh
     global s
-    global sg
     fake_config = fixtures.FakeConfig()
     conf = fake_config.get_node_info()
-    conf["compute"]["guest-agent"] = True
+
     conf["compute"]["networks"] = [
         {
             "device": "e1000",
             "id": "e1000.0",
-            "mac": "00:60:16:9c:50:6a"
-        }
-    ]
-    conf["compute"]["networks"][0]["port_forward"] = [
+            "mac": "00:60:16:9c:ff:6a",
+            "network_mode": "nat",
+            "port_forward": [
+                {
+                    "outside": 2222,
+                    "inside": 22,
+                    "protocal": "tcp"
+                }
+            ]
+        },
         {
-            "outside": 2222,
-            "inside": 22,
-            "protocal": "tcp"
+            "device": "e1000",
+            "id": "e1000.1",
+            "network_mode": "nat",
+            "mac": target_mac,
         }
     ]
     conf["compute"]["storage_backend"] = [
@@ -67,8 +70,6 @@ def start_node():
             ]
         },
     ]
-    conf["compute"]["guest-agent"] = True
-
     node = model.CNode(conf)
     node.init()
     node.precheck()
@@ -85,77 +86,31 @@ def start_node():
 
     s.send(json.dumps(payload_enable_qmp))
     s.recv()
-    # second sg: unixsocket guest.agt
-    path_guestagt = os.path.join(node.workspace.get_workspace(), "guest.agt")
-    sg = UnixSocket(path_guestagt)
-    sg.connect()
 
-    payload_test_ping = {
-        "execute": "guest-ping"
-    }
-
-    sg.send(json.dumps(payload_test_ping))
-
-    # wait until system is ready for ssh.
-    ssh = sshclient.SSH(host="127.0.0.1", username="root", password="root", port=2222)
-    ssh.wait_for_host_up()
+    ssh = prepare_ssh()
 
 
 def stop_node():
-    global s
-    global sg
     global conf
+    if conf is None:
+        return
     s.close()
-    sg.close()
+    ssh_close(ssh)
     node = model.CNode(conf)
     node.init()
     node.stop()
+    conf = None
     node.terminate_workspace()
-    conf = {}
 
 
 def get_interface_name():
-    global inface
-    global ssh
-    with open("ifg_info.txt", "w+") as ff:
-        status, output = ssh.exec_command("ip link")
-        ff.write(output)
-        ff.seek(0, 0)
-        for str0 in ff.readlines():
-            if "00:60:16:9c:50:6a" in str0:
-                break
-            str1 = str0.split()
-            # in order to delete ":"
-            inface = str1[1][0:-1]
-    os.remove('ifg_info.txt')
-
-
-def guest_exec_cmd_step(cmd_exec, cmd_status, sfd):
-
-    sfd.send(json.dumps(cmd_exec))
-    # wait for data all
-    time.sleep(1)
-    teststr = json.loads(sfd.recv())
-    cmd_status["arguments"]["pid"] = teststr["return"]["pid"]
-
-    sfd.send(json.dumps(cmd_status))
-    stest = ""
-    tcount = 0
-    while True:
-        tcount += 1
-        time.sleep(1)
-        rdata = ""
-        rdata = sfd.recv()
-        stest += rdata
-        if ("\"exited\": true" in stest) or (tcount > 100):
+    inface = "lo"
+    ret = ssh_exec(ssh, "for f in /sys/class/net/*/address; do echo -n $f' '; cat $f; done")
+    for line in ret.split('\n'):
+        if target_mac in line:
+            inface = line.split('/')[4]
             break
-
-    teststr1 = json.loads(stest)
-    ifdata = (teststr1.get('return')).get("out-data")
-    pidinfo = base64.b64decode(ifdata)
-    print(pidinfo)
-
-    return pidinfo
+    return inface
 
 
 class test_link_status_error_inject(unittest.TestCase):
@@ -169,71 +124,51 @@ class test_link_status_error_inject(unittest.TestCase):
         stop_node()
 
     def test_status_code_error_inject(self):
-        status_code_error1 = {
+        cmd_nic_down = {
             "execute": "set_link",
             "arguments": {
-                "name": "e1000.0",
+                "name": "e1000.1",
                 "up": False
             }
         }
-        status_code_error2 = {
+        cmd_nic_up = {
             "execute": "set_link",
             "arguments": {
-                "name": "e1000.0",
+                "name": "e1000.1",
                 "up": True
             }
         }
-        guest_cmd_ifconfig = {
-            "execute": "guest-exec",
-            "arguments": {
-                "path": "/sbin/ifconfig",
-                "capture-output": True
-            }
-        }
-        guest_cmd_pid = {
-            "execute": "guest-exec-status",
-            "arguments": {
-                "pid": None
-            }
-        }
-
         # defore link down interface ,use ssh do some prepare works
-        get_interface_name()
-        status, output = ssh.exec_command(" ifconfig")
-        self.assertIn("{}: flags=4163".format(inface), output, "system start and interface down ")
+        nic_name = get_interface_name()
+        print("Found target nic: {}".format(nic_name))
+        output = ssh_exec(ssh, "dhclient {}".format(nic_name))
+        output = ssh_exec(ssh, "ifconfig {}".format(nic_name))
+        print(output)
+        self.assertIn(",RUNNING,", output, "NIC {} is not up yet".format(nic_name))
 
-        status, output = ssh.exec_command(" wget {} -O infrasim-qemu_2.10.1-ubuntu.deb".format(QEMU_PATH))
-        self.assertFalse(status)
-        status, output = ssh.exec_command(" sudo dpkg -i infrasim-qemu_2.10.1-ubuntu.deb")
-        self.assertFalse(status)
-
-        status, output = ssh.exec_command(" rm  infrasim-qemu_2.10.1-ubuntu.deb")
-        self.assertFalse(status)
-        status, output = ssh.exec_command(" touch qemu-ga.pid")
-        self.assertFalse(status)
-        status, output = ssh.exec_command(" sudo qemu-ga -m isa-serial -p /dev/ttyS3 -t /tmp -f qemu-ga.pid -d")
-        self.assertFalse(status)
         # down interface and get status
-        s.send(json.dumps(status_code_error1))
+        s.send(json.dumps(cmd_nic_down))
         s.recv()
-        # must wait for cmd exec ok ,otherwise the status will not right
-        count = 0
-        while ssh.connected():
-            time.sleep(1)
-            self.assertNotEqual(5, count, "link down operation did't work")
-            count += 1
 
-        ifginfo = guest_exec_cmd_step(guest_cmd_ifconfig, guest_cmd_pid, sg)
-        self.assertIn("{}: flags=4099".format(inface), ifginfo, "set link down fail")
+        # try 10 times,
+        for _ in range(0, 10):
+            output = ssh_exec(ssh, "ifconfig {}".format(nic_name))
+            if ",RUNNING," not in output:
+                break
+            sleep(1)
+        else:
+            print(output)
+            self.fail("NIC {} is not down".format(nic_name))
+
         # set interface up and get status
-        s.send(json.dumps(status_code_error2))
+        s.send(json.dumps(cmd_nic_up))
         s.recv()
 
-        count = 0
-        while not ssh.connect():
-            time.sleep(1)
-            self.assertNotEqual(5, count, "link up operation did't work")
-            count += 1
-
-        ifginfo = guest_exec_cmd_step(guest_cmd_ifconfig, guest_cmd_pid, sg)
-        self.assertIn("{}: flags=4163".format(inface), ifginfo, "set link up fail")
+        for _ in range(0, 10):
+            output = ssh_exec(ssh, "ifconfig {}".format(nic_name))
+            if ",RUNNING," in output:
+                break
+            sleep(1)
+        else:
+            print(output)
+            self.assertIn("NIC {} is not up again".format(nic_name))
